dd734a0b44477679bbc6a3f51639667a2a1ed91b
RaulGonzal <Raulgonzalc@yahoo.com>
2024-02-01 21:48:13 -0500

    register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from '../middleware/asyncHandler.js'
 import User from '../models/userModel.js'
-import jwt from 'jsonwebtoken'
+import generateToken from '../utils/generateToken.js'
+
 // @desc    auth user & get token
 // @route   POST /api/users/login
 // @access  Public
@@ -10,17 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
     const user = await User.findOne({email})
 
     if (user && (await user.matchPassword(password))) {
-        const token = jwt.sign({ userID: user._id}, process.env.JWT_SECRET, {
-            expiresIn: '7d'
-        })
-
-        // set jwt as http-only cookie
-        res.cookie('jwt', token, {
-            httpOnly: true, 
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000     // 7 days
-        })
+        generateToken(res, user._id)
 
         res.json({
             _id: user._id,
@@ -40,7 +31,34 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-   res.send('Register user')
+   const {name, email, password} = req.body
+
+   const userExists = await User.findOne({ email })
+
+   if(userExists){
+    res.status(400)
+    throw new Error('User already exists')
+   }
+
+   const user = await User.create({
+    name, 
+    email, 
+    password
+   })
+
+   if (user){
+    generateToken(res, user._id)
+    res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin
+    })
+   }
+   else {
+    res.status(400)
+    throw new Error('Invalid user data')
+   }
 })
 
 // @desc    logout user
@@ -52,7 +70,7 @@ const logoutUser = asyncHandler(async (req, res) => {
         expires: new Date(0),
     })
 
-    res.status(200).json({message: 'Logget out successfully'})
+    res.status(200).json({message: 'Logged out successfully'})
 })
 
 // @desc    get user profile
