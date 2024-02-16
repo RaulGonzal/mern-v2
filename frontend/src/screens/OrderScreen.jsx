4e19e4da49a93c9b36d3204c00c8c1a9a5d59fb3
RaulGonzal <Raulgonzalc@yahoo.com>
2024-02-16 14:37:06 -0500

    comment out test button

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -61,12 +61,12 @@ const OrderScreen = () => {
               }
             
               // TESTING ONLY! REMOVE BEFORE PRODUCTION
-              async function onApproveTest() {
+              /*async function onApproveTest() {
                 await payOrder({ orderId, details: { payer: {} } });
                 refetch();
                 
                 toast.success('Order is paid');
-              }
+              }*/
             
               function onError(err) {
                 toast.error(err.message);
