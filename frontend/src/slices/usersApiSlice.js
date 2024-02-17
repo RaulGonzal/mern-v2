21bfb8c8bf3aec015dca42a6a1d02f86a4817e2b
RaulGonzal <Raulgonzalc@yahoo.com>
2024-02-16 23:54:55 -0500

    edit user

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -66,4 +66,6 @@ export const {
     useRegisterMutation, 
     useProfileMutation,
     useGetUsersQuery,
-    useDeleteUserMutation,} = usersApiSlice
+    useDeleteUserMutation,
+    useGetUserDetailsQuery,
+    useUpdateUserMutation,} = usersApiSlice
