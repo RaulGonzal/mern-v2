de482e9a4021dfa95840cb6c3b496974d83d0cd1
RaulGonzal <Raulgonzalc@yahoo.com>
2024-02-22 23:17:00 -0500

    search box

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,9 @@ import {FaShoppingCart, FaUser} from 'react-icons/fa';
 import {useSelector, useDispatch} from 'react-redux'
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import {logout} from '../slices/authSlice'
+import SearchBox from './SearchBox';
 import {LinkContainer} from 'react-router-bootstrap'
+import { resetCart } from '../slices/cartSlice';
 //import logo from '../assets/logo.png'
 
 const Header = () => {
@@ -20,6 +22,7 @@ const Header = () => {
         try{
             await logoutApiCall().unwrap()
             dispatch(logout())
+            dispatch(resetCart());
             navigate('/login')
         } catch(err){
             console.log(err)
@@ -38,6 +41,7 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='ms-auto'>
+                        <SearchBox />
                         <LinkContainer to='/cart'>
                             <Nav.Link>
                                 <FaShoppingCart /> Cart
